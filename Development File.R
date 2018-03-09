9f8f80a07f14b1e25ba4f0dfbc9ac6bcf4202b91
JackPloshnick <36051309+JackPloshnick@users.noreply.github.com>
2018-03-08 19:00:46 -0600

    creates Rasch Class

---
+++ Development File.R
@@ -0,0 +1,34 @@
+#Start time 6:52 PM on 3/3/18
+
+a= c(1,2,3,4,5,4,3,2,1) #difficulty
+
+y= c(1,1,1,0,0,1,1,0,1) #answer. 1 is correct. 0 is incorrect 
+
+### creating Rasch class
+
+setClass(Class="Rasch",  #Sets S4 class of Rasch
+         representation = representation(
+           name = "character", # three slots as specified in problem set
+           a_value = "numeric", 
+           y_j_value = "numeric"
+         ),
+         prototype = prototype(
+           name = c(), #default values are empty 
+           a_value=  c(),
+          y_j_value  = c()
+         )
+)
+
+setValidity("Rasch", function(object){ #do this later 
+
+})
+
+
+setMethod("initialize", "Rasch", function(.Object, ...) { #initilize method 
+  value = callNextMethod()
+  validObject(value)
+  return(value)
+})
+
+###########
+
